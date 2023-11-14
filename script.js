493742dee7a1f23a80520001c00110436dee9bf5
Arijan1 <116945362+Arijan1@users.noreply.github.com>
2023-11-14 23:24:48 +0100

    promjene
    usluge preuredene u slider, kontakt form updejtan. Ostalo jos 3 sekcije

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+var splide = new Splide(".splide", {
+  type: "loop",
+});
 
-
+splide.mount();
 var scroll = new SmoothScroll('a[href*="#"]', {
-    speed: 1e2,
-    speedAsDuration: !0,
-  });
+  speed: 1e2,
+  speedAsDuration: !0,
+});
 
-  var menuBtn = document.querySelector(".menu-btn"),
+var menuBtn = document.querySelector(".menu-btn"),
   closeBtn = document.querySelector(".close-btn"),
   navLogo = document.querySelector(".nav-logo"),
   navigationList = document.getElementById("navigationLista"),
@@ -14,14 +17,13 @@ function pokazivanje() {
   (navigationList.style.display = "flex"),
     (menuBtn.style.display = "none"),
     (closeBtn.style.display = "block"),
-    (overlay.classList.add("overlay-active"));
+    overlay.classList.add("overlay-active");
 }
 function zatvaranje() {
-    (navigationList.style.display = "none"),
+  (navigationList.style.display = "none"),
     (menuBtn.style.display = "block"),
     (closeBtn.style.display = "none"),
-    (overlay.classList.remove("overlay-active"));
+    overlay.classList.remove("overlay-active");
 }
 menuBtn.addEventListener("click", pokazivanje),
   closeBtn.addEventListener("click", zatvaranje);
-
